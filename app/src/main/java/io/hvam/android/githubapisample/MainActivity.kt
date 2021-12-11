0414055853b69395da220928a37e5ccc3ba4183e
Danny Hvam <dkhvam@gmail.com>
2021-12-11 21:10:42 +0100

    init commit
    This is the default Android Studio project setup

---
+++ app/src/main/java/io/hvam/android/githubapisample/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.hvam.android.githubapisample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.hvam.android.githubapisample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/hvam/android/githubapisample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.hvam.android.githubapisample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
